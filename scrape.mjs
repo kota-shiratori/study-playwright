8b0da81b2c3a79fa5f65feb473b374236538bba3
kota_shiratori <swan.kouta@gmail.com>
2024-05-25 23:31:20 +0900

    🎉 playwright読み込み成功

---
+++ scrape.mjs
@@ -0,0 +1,15 @@
+import { chromium } from "playwright";
+
+//ブラウザが立ち上がる
+const browser = await chromium.launch();
+
+//新しいページを立ち上げる
+const page = await browser.newPage();
+
+//どこのページに遷移するか
+await page.goto("https://swan-room.com/");
+
+//ページのHTML要素が取れてくる
+const htmlStr = await page.content();
+
+console.log(htmlStr);
