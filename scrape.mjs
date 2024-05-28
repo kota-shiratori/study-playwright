b4298b92c8399e7a1c302057354ca65c8aa63718
kota_shiratori <swan.kouta@gmail.com>
2024-05-28 21:44:58 +0900

    🎉

--- scrape.mjs
+++ scrape.mjs
@@ -1,48 +1,46 @@
 import { chromium } from "playwright";
 
 (async () => {
-  //ブラウザが立ち上がる
-  const browser = await chromium.launch({ headless: false, slowMo: 500 });
-
-  //新しいページを立ち上げる
+  const browser = await chromium.launch({ headless: true });
   const page = await browser.newPage();
 
-  //どこのページに遷移するか
   await page.goto(
     "https://search.rakuten.co.jp/search/mall/%E3%83%A2%E3%83%AB%E3%82%B8%E3%83%A5+%E3%83%86%E3%83%B3%E3%83%88%E3%82%B5%E3%82%A6%E3%83%8A/"
   );
+  await page.waitForSelector(".dui-cards.searchresultitems");
 
   const itemsLocator = page.locator(
     ".dui-cards.searchresultitems .dui-card.searchresultitem"
   );
   const count = await itemsLocator.count();
+  const items = [];
+
   for (let i = 0; i < count; i++) {
-    const title = await itemsLocator
-      .nth(i)
+    const item = itemsLocator.nth(i);
+    const title = await item
       .locator(".title-link-wrapper--2sUFJ")
-      .innerText();
-    const price = await itemsLocator
-      .nth(i)
+      .innerText()
+      .catch(() => "N/A");
+    const price = await item
       .locator(".price--OX_YW")
-      .innerText();
-    const score = await itemsLocator
-      .nth(i)
+      .innerText()
+      .catch(() => "N/A");
+    const score = await item
       .locator(".score")
       .innerText()
       .catch(() => "N/A");
-    const legend = await itemsLocator
-      .nth(i)
+    const legend = await item
       .locator(".legend")
       .innerText()
       .catch(() => "N/A");
-    const link = await itemsLocator
-      .nth(i)
+    const link = await item
       .locator(".image-wrapper--1ROeD a")
       .getAttribute("href")
       .catch(() => "N/A");
-    console.log({ title, price, link, score, legend });
+    items.push({ title, price, link, score, legend });
   }
 
-  //ブラウザを閉じる
+  console.log(items);
+
   await browser.close();
 })();
